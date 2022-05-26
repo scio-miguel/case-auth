add6e3823646a09158200bda92d720eefa992ced
scio-miguel <armando.zegarra@sciodev.com>
2022-05-26 13:04:33 -0500

    env

--- config/database.js
+++ config/database.js
@@ -2,8 +2,13 @@ const { Sequelize } = require("sequelize");
 
 require("dotenv").config();
 
-const sequelize = new Sequelize("case_auth", "ackercoder", "", {
-  host: "localhost",
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
   dialect: "postgres",
 });
 
